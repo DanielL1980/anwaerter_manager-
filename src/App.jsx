e03ec262bbb7556e44b7280ad9ba8f4c7676e1fa
DanielL1980 <mail.daniell@gmx.de>
2026-03-21 08:26:54 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
+import LehrprobeDetail from './pages/LehrprobeDetail'; // <-- NEU
 
 function App() {
   return (
@@ -8,6 +9,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
+          {/* NEUE ROUTE FÜR DIE DETAILSEITE */}
+          <Route path="/lehrprobe/:id" element={<LehrprobeDetail />} />
         </Route>
       </Routes>
     </BrowserRouter>
