2b58fd5f0a8be492a9ed1dd6252e5311782571ee
DanielL1980 <mail.daniell@gmx.de>
2026-03-21 08:17:34 +0100

    Create kriterien.js

---
+++ src/data/kriterien.js
@@ -0,0 +1,43 @@
+export const BEWERTUNGSKRITERIEN = [
+  {
+    id: 'sachkompetenz',
+    titel: 'Sachkompetenz',
+    punkte: [
+      { id: 'stoffauswahl', text: 'Stoffauswahl und -aufbereitung' },
+      { id: 'fachwissen', text: 'Fachwissen und -sicherheit' },
+      { id: 'praxisbezug', text: 'Praxisbezug und Relevanz' },
+      { id: 'rechtsgrundlagen', text: 'Bezug zu Rechtsgrundlagen' },
+    ],
+  },
+  {
+    id: 'methodenkompetenz',
+    titel: 'Methodenkompetenz',
+    punkte: [
+      { id: 'zielsetzung', text: 'Klare Zielsetzung und "Roter Faden"' },
+      { id: 'methodenvielfalt', text: 'Methodenvielfalt und -wechsel' },
+      { id: 'medien', text: 'Medieneinsatz (Tafel, Modell, digital, ...)' },
+      { id: 'zeitmanagement', text: 'Zeitmanagement' },
+      { id: 'aktivierung', text: 'Aktivierung der Fahrschüler' },
+    ],
+  },
+  {
+    id: 'sozialkompetenz',
+    titel: 'Sozialkompetenz',
+    punkte: [
+      { id: 'kommunikation', text: 'Kommunikation und Gesprächsführung' },
+      { id: 'ansprechbarkeit', text: 'Ansprechbarkeit und Eingehen auf Fragen' },
+      { id: 'gruppenklima', text: 'Umgang mit der Gruppe, Lernatmosphäre' },
+      { id: 'feedback', text: 'Umgang mit Feedback und Störungen' },
+    ],
+  },
+  {
+    id: 'personalkompetenz',
+    titel: 'Personalkompetenz',
+    punkte: [
+      { id: 'auftreten', text: 'Auftreten und Körpersprache' },
+      { id: 'sprache', text: 'Sprache und Ausdrucksweise' },
+      { id: 'motivation', text: 'Motivation und Engagement' },
+      { id: 'reflexion', text: 'Reflexionsfähigkeit' },
+    ],
+  },
+];
